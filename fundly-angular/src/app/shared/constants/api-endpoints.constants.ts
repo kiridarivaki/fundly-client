ef2a2236868bda237b8884a6ffba5d0af1557a62
Kyriaki Darivaki <t8210030@aueb.gr>
2026-03-01 13:53:13 +0200

    add path and api mappings

---
+++ fundly-angular/src/app/shared/constants/api-endpoints.constants.ts
@@ -0,0 +1,13 @@
+export const API_ENDPOINTS = {
+    USER: {
+        BASE: 'user',
+        REGISTER: '/register',
+        PROFILE: '/profile/:id'
+    },  
+    GOAL: {
+        BASE: 'goal'    
+    },
+    EXPENSE: {
+        BASE: 'expense'
+    }
+};
---
+++ fundly-angular/src/app/shared/constants/paths.constants.ts
@@ -0,0 +1,11 @@
+export const HOME_PATHS = {
+    BASE: '', 
+    HOME: '',
+    ABOUT: 'about'
+}
+
+export const AUTH_PATHS = {
+    BASE: 'auth',
+    LOGIN: 'login',
+    REGISTER: 'register'
+}
