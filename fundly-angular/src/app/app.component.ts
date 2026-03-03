13738f54f22bb2168221cb5146fe44f1748e3d52
Kyriaki Darivaki <t8210030@aueb.gr>
2026-03-03 15:35:27 +0200

    add transloco localization

--- fundly-angular/src/app/app.component.ts
+++ fundly-angular/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslocoDirective } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslocoDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- fundly-angular/src/app/app.config.ts
+++ fundly-angular/src/app/app.config.ts
@@ -1,12 +1,23 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { appRoutes } from './app.routes';
+import { TranslocoHttpLoader } from './transloco-loader';
+import { provideTransloco } from '@jsverse/transloco';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes),
-    provideHttpClient()
+    provideRouter(appRoutes),
+    provideHttpClient(), provideHttpClient(), provideTransloco({
+        config: { 
+          availableLangs: ['en', 'gr'],
+          defaultLang: 'en',
+          // Remove this option if your application doesn't support changing language in runtime.
+          reRenderOnLangChange: true,
+          prodMode: !isDevMode(),
+        },
+        loader: TranslocoHttpLoader
+      })
   ]
 };
