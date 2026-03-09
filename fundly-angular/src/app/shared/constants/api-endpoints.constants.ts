b0c959a7814b829e9ae7208bdeae5de34f76df80
Kyriaki Darivaki <t8210030@aueb.gr>
2026-03-09 20:21:06 +0200

    fix api mappings

--- fundly-angular/src/app/shared/constants/api-endpoints.constants.ts
+++ fundly-angular/src/app/shared/constants/api-endpoints.constants.ts
@@ -2,12 +2,26 @@ export const API_ENDPOINTS = {
     USER: {
         BASE: 'user',
         REGISTER: '/register',
-        PROFILE: '/profile/:id'
+        PROFILE: '/profile/{id}',
+        EDIT: '/edit/{id}'
     },  
     GOAL: {
         BASE: 'goal'    
     },
     EXPENSE: {
-        BASE: 'expense'
+        BASE: 'expense',
+        MANAGER: '/expense-manager',
+        ADD: '/add',
+        DETAILS: '/details/{id}',
+        EDIT: '/edit/{id}',
+        DELETE: '/delete/{id}'
+    },
+    EXPENSE_CATEGORY: {
+        BASE: 'expense-category',
+        LIST: '/list',
+        ADD: '/add',
+        DETAILS: '/details/{id}',
+        EDIT: '/edit/{id}',
+        DELETE: '/delete/{id}'
     }
 };
